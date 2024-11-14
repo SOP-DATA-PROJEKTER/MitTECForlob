dd8038c86034afbc204006ef40998e91bcf8fd7f
Emil Isak Lundqvist <emil372r@elev.tec.dk>
2024-11-14 12:10:07 +0100

    added generic repo

---
+++ Logic/Models Logic/Table Repo/GenericRepo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models_Logic.Table_Repo
+{
+    public abstract class GenericRepo<T>
+    {
+        DBcontext context;
+
+
+        public GenericRepo(DBcontext c)
+        {
+            context = c;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            try
+            {
+
+                await context.AddAsync(entity);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return entity;
+        }
+
+
+
+        public async Task<T> Update(T entity)
+        {
+            try
+            {
+
+                context.Update(entity);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return entity;
+        }
+
+
+        public async Task<T> Delete(T entity)
+        {
+            try
+            {
+
+                context.Remove(entity);
+                await context.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return entity;
+        }
+
+
+    }
+}
